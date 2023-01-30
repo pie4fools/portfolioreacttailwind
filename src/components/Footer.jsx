d619a144db9804268dbe692804e0943535ee3e90
pie4fools <64813630+pie4fools@users.noreply.github.com>
2023-01-30 13:11:57 -0600

    Fixed button sizing at the bottom & resume but

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,28 +13,28 @@ const Footer = () => {
                 <img className='scale-75 align-center' src={chatgptpic} alt="Screenshot of ChatGPT Clone" />
                 <p>Using OpenAIAPI, send GET requests from the front end and listens for POST requests. The ChatGPT API replies with a post request to the frontend, where the content is displayed in text format.</p>
                 <a href='https://chatgptclone-lilac.vercel.app/' target='_blank' rel='noreferrer' className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto py-2.5 px-6 lg:mr-2 sm:mr-0 text-white hover:bg-black focus:ring focus:ring-grey-300'>Visit</a>
-                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto pb-0.5 text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
+                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
             </div>
             <div className='text-center shadow-lg p-10 rounded-xl my-10 mx-10'>
                 <h1>General E-Commerce Site</h1>
                 <img className='scale-75 align-center' src={creditcardpic} alt="Screenshot credit card website" />
                 <p>Simple react site for a credit card company. Styled components with a register/login ready to connect to backend.</p>
                 <a href='https://master--beautiful-zuccutto-11d061.netlify.app/' target='_blank' rel='noreferrer' className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto py-2.5 px-6 sm:mr-0 lg:mr-2 text-white hover:bg-black focus:ring focus:ring-grey-300'>Visit</a>
-                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto pb-0.5 text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
+                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
             </div>
             <div className='text-center shadow-lg p-10 rounded-xl my-10 mx-10'>
                 <h1>Dictionary API</h1>
                 <img className='scale-75 align-center' src={dictionaryapi} alt="Screenshot of dictionary site" />
                 <p>Searches for a word in the dictionaryapi.dev API and displays the results on the page. Sets audio source attribute with a pronunciation of the word from the API</p>
                 <a href='https://master--chipper-salmiakki-0518f5.netlify.app/' target='_blank' rel='noreferrer' className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto py-2.5 px-6 sm:mr-0 lg:mr-2 text-white hover:bg-black focus:ring focus:ring-grey-300'>Visit</a>
-                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto pb-0.5 text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
+                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
             </div>
             <div className='sm:mb-0 text-center shadow-lg p-10 rounded-xl lg:my-10 mx-10'>
                 <h1 className='mb-4'>Pristine Cleaning</h1>
                 <img className='scale-100 mb-6' src={pristinecleanpic} alt="Screenshot of pristinecleaningph.com" />
                 <p>This is my first website completed for a client. This is a simple project using HTML/CSS and minimal JS.</p>
                 <a href='http://www.pristinecleaningph.com/' target='_blank' rel='noreferrer' className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto py-2.5 px-6 text-white sm:mr-2 lg:mr-2 hover:bg-black focus:ring focus:ring-grey-300'>Visit</a>
-                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto pb-0.5 text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
+                <button className='bg-[#00dfd4] w-[90px] h-[37.5px] rounded-full font-medium mt-7 mx-auto text-white hover:bg-black focus:ring focus:ring-grey-300'>Source</button>
             </div>
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white z-10'>
         <h1 className='w-full text-3xl font-bold text-[#00dfd4]'>PH</h1>
         <ul className='flex'>
-            <button className='bg-[#00dfd4] w-[80px] h-[35.5px] rounded-md font-medium mt-7 mx-auto py-0.5 z-10 text-black hover:bg-white focus:ring focus:ring-white-300'>Resume</button>
+            <button className='bg-[#00dfd4] w-[80px] h-[34px] rounded-md font-medium mt-7 mx-auto py-0.5 z-10 text-black hover:bg-white focus:ring focus:ring-white-300'>Resume</button>
             <div className="z-10">
             <a
             href="https://www.linkedin.com/in/piercen-hughes-617044188/"
